e411eae2a3eabb29556146c24cdf28e24846a57f
Adeleye-jamiu <oluwaloseyifunmiadeleye1@gmail.com>
2022-02-10 09:19:24 +0100

    START: add project boilerplate

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Task-List-API</title>
+        
+    </head>
+    <body>
+        <div>
+            Task List API
+        </div>
+    </body>
+</html>
